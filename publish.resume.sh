27643639b990459f341692d2361334d2e4fc18ed
Shunfa Xu <sfxu@thoughtworks.com>
2020-02-11 18:42:46 +0700

    seperate resume blog and personal blog

--- publish.resume.sh
+++ publish.resume.sh
@@ -2,10 +2,10 @@
 
 # run below commands in your jekyll root directory
 
-JEKYLL_ENV=production jekyll build --unpublished
+JEKYLL_ENV=production jekyll build
 cd _site
 git init
-git remote add origin git@github.com:goooooouwa/me.git
+git remote add origin git@github.com:goooooouwa/goooooouwa.github.io.git
 git add .
 git commit -m "jekyll build at $(date)"
 git push origin master -f
