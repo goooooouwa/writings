d0701ec977bb223ffee3f88e8c6aae69a7879d74
Shunfa Xu <sfxu@thoughtworks.com>
2020-02-14 22:37:40 +0700

    Update publish.blog.sh

--- scripts/publish.blog.sh
+++ scripts/publish.blog.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo $WORKSPACE
-cd $WORKSPACE/_site
+echo $1
+echo $2
+cd $1/_site
 git init
 git config --local user.email "sfxu@thoughtworks.com"
 git config --local user.name "Shunfa Xu"
-git remote add origin https://goooooouwa:$PERSONAL_ACCESS_TOKEN@github.com/goooooouwa/blog.git
+git remote add origin https://goooooouwa:$2@github.com/goooooouwa/blog.git
 git add .
 git commit -m "jekyll build at $(date)"
 git push origin master -f
