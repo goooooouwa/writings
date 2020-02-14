d6c8db77dc3d3be5e8f433c0278e45b48befa2c7
Shunfa Xu <sfxu@thoughtworks.com>
2020-02-14 23:30:18 +0700

    Update publish.resume.sh

--- scripts/publish.resume.sh
+++ scripts/publish.resume.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# run below commands in your jekyll root directory
-
-rm -rf _site
+cd $1
 JEKYLL_ENV=production jekyll build --config _config.yml,_config.prod.resume.yml
-cd _site
+
+cd $1/_site
 git init
-git remote add origin git@github.com:goooooouwa/goooooouwa.github.io.git
+git config --local user.email "action@github.com"
+git config --local user.name "Github Action"
+git remote add origin https://goooooouwa:$2@github.com/goooooouwa/goooooouwa.github.io.git
 git add .
 git commit -m "jekyll build at $(date)"
 git push origin master -f
-rm -rf .git
