66e93ab5d8bd22542c4e64eac50c6777f0ef61e4
Shunfa Xu <sfxu@thoughtworks.com>
2020-03-06 14:14:21 +0700

    move resume to a project page

--- scripts/publish.resume.sh
+++ scripts/publish.resume.sh
@@ -5,9 +5,9 @@ JEKYLL_ENV=production jekyll build --config _config.yml,_config.prod.resume.yml
 
 cd $1/_site
 git init
-git config --local user.email "action@github.com"
-git config --local user.name "Github Action"
-git remote add origin https://goooooouwa:$2@github.com/goooooouwa/goooooouwa.github.io.git
+git config --local user.email "sfxu@thoughtworks.com"
+git config --local user.name "Shunfa Xu"
+git remote add origin https://goooooouwa:$2@github.com/goooooouwa/resume.git
 git add .
 git commit -m "jekyll build at $(date)"
 git push origin master -f
