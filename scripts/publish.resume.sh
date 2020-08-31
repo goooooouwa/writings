cf15a4f4d4482d7dd3f092a6ff752549755a4e49
Shunfa Xu <sfxu@thoughtworks.com>
2020-08-31 17:11:21 +0800

    use Github Action email

--- scripts/publish.resume.sh
+++ scripts/publish.resume.sh
@@ -5,8 +5,8 @@ JEKYLL_ENV=production jekyll build --config _config.yml,_config.resume.yml
 
 cd $1/_site
 git init
-git config --local user.email "sfxu@thoughtworks.com"
-git config --local user.name "Shunfa Xu"
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
 git remote add origin https://goooooouwa:$2@github.com/goooooouwa/resume.git
 git add .
 git commit -m "jekyll build at $(date)"
